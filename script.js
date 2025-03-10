950c9dc6788679afe69259c0d3deee0ad04bce8d
Shasan634 <132423774+Shasan634@users.noreply.github.com>
2025-03-10 07:31:53 -0230

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.querySelectorAll('.card-back').forEach(cardBack => {
-    const imageUrl = cardBack.getAttribute('data-image');
-    cardBack.style.backgroundImage = `url(${imageUrl})`;
-});
